b935f785f810c37e768294aad6def1b99a74981a
volodya4u <volodya4u@hotmail.com>
2026-01-02 01:35:38 +0200

    Add lombok dependency

--- src/main/java/com/appsdeveloperblog/estore/DepositService/handler/DepositRequestedEventHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/DepositService/handler/DepositRequestedEventHandler.java
@@ -1,21 +1,20 @@
 package com.appsdeveloperblog.estore.DepositService.handler;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import com.appsdeveloperblog.payments.ws.core.events.DepositRequestedEvent;
+import com.appsdeveloperblog.ws.core.events.DepositRequestedEvent;
 
+@Slf4j
 @Component
 @KafkaListener(topics = "deposit-money-topic", containerFactory = "kafkaListenerContainerFactory")
 public class DepositRequestedEventHandler {
-	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
 	@KafkaHandler
 	public void handle(@Payload DepositRequestedEvent depositRequestedEvent) {
-		LOGGER.info("Received a new deposit event: {} ", depositRequestedEvent.getAmount());
+		log.info("Received a new deposit event: {} ", depositRequestedEvent.getAmount());
 	}
 }
